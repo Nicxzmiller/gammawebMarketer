45d41aee9fca924f23fdcbe38af497f9faf5a61f
nicxzmiller <nicxzmiller2@gmail.com>
2019-10-10 11:49:02 +0200

    installing additional dependencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
+
 require('./bootstrap');
+
+window.Vue = require('vue');
+import Buefy from 'buefy';
+
+Vue.use(Buefy);
+
+
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+//
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+
+
+// const app = new Vue({
+//     el: '#app',
+// });
